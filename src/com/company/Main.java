28bdc252b0eb0bf9ccbcf6b1fc0ac019cf94ba36
Kuba Radwanski <akrioon@gmail.com>
2020-06-18 16:42:33 +0200

    Fixed bugs: data implemented as hashmap instead of list of lists, handled errors with missing vales

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,28 +10,29 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void printTen(List<List<String>> data, List<String> columns){
-        int limit;
-        if(data.size()>10) limit=10; else limit=data.size();
-        for(int i=0;i<limit;i++)
-       {
-           for(int j=0;j<columns.size();j++) {
-               System.out.println(columns.get(j)+":"+data.get(i).get(j));
-           }
-           System.out.println("-------------------");
-       }
+
+    public static void printTen(HashMap<String, List<String>> data, List<String> columns) {
+        int size=0;
+        for(String item:data.keySet())
+        {
+            for(int j=0;j<columns.size();j++) {
+                System.out.println(columns.get(j)+":"+data.get(item).get(j));
+            }
+            System.out.println("-------------------");
+            size++;
+            if(size==10)break;
+        }
     }
 
-    public static void printNumberOfObjects(List<List<String>> data)
-    {
+    public static void printNumberOfObjects(HashMap<String, List<String>> data) {
         System.out.println("Number of objects:"+data.size());
     }
 
-    public static void printAllBigger(List<List<String>> data,List<String> columns, int id) throws CsvException {
+    public static void printAllBigger(HashMap<String,List<String>> data,List<String> columns, int id) throws CsvException {
         int indexOfIdColumn=columns.indexOf("id");
         if(indexOfIdColumn==-1)throw new CsvException("no column with the name:id");
         else  {
-            data.stream().sorted(Comparator.comparingInt(e-> Integer.parseInt(e.get(indexOfIdColumn))))
+            data.values().stream().sorted(Comparator.comparingInt(e-> Integer.parseInt(e.get(indexOfIdColumn))))
                          .filter(idHigherThan(id,indexOfIdColumn)).forEach(System.out::println);
         }
     }
@@ -40,12 +41,13 @@ public class Main {
         return p -> Integer.parseInt(p.get(indexOfIdColumn))>id;
     }
 
-    public static void printNamesAndOccurrences(List<List<String>> data, List<String> columns) throws CsvException {
+
+    public static void printNamesAndOccurrences(HashMap<String,List<String>> data, List<String> columns) throws CsvException {
         int indexOfNameColumn = columns.indexOf("name");
         if (indexOfNameColumn == -1) throw new CsvException("no column with the name:name");
         else {
             HashMap<String, Integer> result = new HashMap<>();
-            List<String> names = data.stream().map(e -> e.get(indexOfNameColumn)).collect(Collectors.toList());
+            List<String> names=data.values().stream().map(e -> e.get(indexOfNameColumn)).collect(Collectors.toList());
             for (String name : names) {
                 if (!result.containsKey(name)) result.put(name, 1);
                 else result.put(name, result.get(name) + 1);
@@ -55,44 +57,51 @@ public class Main {
     }
 
 
-    public static void printNameAndCountParents(List<List<String>> data,List<String> columns,int id) throws CsvException {
+
+
+    public static void printNameAndCountParents(HashMap<String, List<String>> data, List<String> columns, String id) throws CsvException {
         int indexOfNameColumn=columns.indexOf("name");
         int indexOfParentIdColumn=columns.indexOf("parent_id");
         int numberOfParents=0;
         String name;
         if (indexOfNameColumn == -1) throw new CsvException("no column with the name:name");
-        if(id<1) throw new CsvException("missing or wrong id!!!");
-        else name=data.get(id-1).get(indexOfNameColumn);
+        if(!data.containsKey(id)) throw new CsvException("missing or wrong id!!!");
+        else name=data.get(id).get(indexOfNameColumn);
         if (indexOfParentIdColumn == -1) throw new CsvException("no column with the name:parent_id");
-            else
+
+        else
         {
-            int parentId=Integer.parseInt(data.get(id-1).get(indexOfParentIdColumn));
-            while(parentId!=0&&Integer.parseInt(data.get(parentId-1).get(indexOfParentIdColumn))!=parentId)
+            int parentId;
+            if(data.get(id).get(indexOfParentIdColumn).equals(""))throw new CsvException("no parent_id value");
+            else {
+                parentId = Integer.parseInt(data.get(id).get(indexOfParentIdColumn));
+            }
+            while(parentId!=0&&Integer.parseInt(data.get(String.valueOf(parentId)).get(indexOfParentIdColumn))!=parentId)
             {
-                parentId=Integer.parseInt(data.get(parentId-1).get(indexOfParentIdColumn));
+                parentId=Integer.parseInt(data.get(String.valueOf(parentId)).get(indexOfParentIdColumn));
                 numberOfParents++;
             }
         }
         System.out.println(name+" - "+numberOfParents);
     }
 
-    public static void printMediumAge(List<List<String>> data, List<String> columns) throws CsvException {
+    public static void printMediumAge(HashMap<String,List<String>> data, List<String> columns) throws CsvException {
         int indexOfAgeColumn=columns.indexOf("age");
         if (indexOfAgeColumn == -1) throw new CsvException("no column with the name:age");
         else {
-            double mediumValue = data.stream().map(e -> e.get(indexOfAgeColumn)).collect(Collectors.averagingInt(num -> Integer.parseInt(num)));
+            double mediumValue=data.values().stream().map(e -> e.get(indexOfAgeColumn)).collect(Collectors.averagingInt(Integer::parseInt));
             System.out.println("Medium age:" + String.format("%.2f", mediumValue));
         }
     }
 
 
-    public static void printRangesOfAge(List<List<String>> data, List<String> columns) throws CsvException {
+    public static void printRangesOfAge(HashMap<String,List<String>> data, List<String> columns) throws CsvException {
         int indexOfAgeColumn=columns.indexOf("age");
         if (indexOfAgeColumn == -1) throw new CsvException("no column with the name:age");
         else {
-            int[] ages = data.stream().mapToInt(e -> Integer.parseInt(e.get(indexOfAgeColumn))).sorted().toArray();
-            Double max = data.stream().map(e -> Double.parseDouble(e.get(indexOfAgeColumn))).max(Double::compare).get();
-            Double min = data.stream().map(e -> Double.parseDouble(e.get(indexOfAgeColumn))).min(Double::compare).get();
+            int[] ages=data.values().stream().mapToInt(e -> Integer.parseInt(e.get(indexOfAgeColumn))).sorted().toArray();
+            Double max = data.values().stream().map(e -> Double.parseDouble(e.get(indexOfAgeColumn))).max(Double::compare).get();
+            Double min = data.values().stream().map(e -> Double.parseDouble(e.get(indexOfAgeColumn))).min(Double::compare).get();
             Double current = min;
             double[] occurrences = new double[4];
             Double step = (max - min) / 4;
@@ -109,19 +118,28 @@ public class Main {
     static double getPercentOfAllElementsInRange(int[] ages,Double a,Double b){
         return  (double)Arrays.stream(ages).filter(x->(a<x&&x<=b)).count()*100/ages.length;
     }
+    
 
     public static void main(String[] args) {
         List<String> arguments= new ArrayList<>(Arrays.asList(args));
-        List<List<String>>data;
+        HashMap<String,List<String>>data=new HashMap<>();
         String fileName=args[0];
         int numberOfOperation=Integer.parseInt(args[1]);
         int additionalParameter=0;
         if(args.length>2) additionalParameter=Integer.parseInt(args[2]);
+        String currentId;
+        int indexOfIdColumn;
+        String line;
+        String DELIMITER=",";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             List<String> columns=Arrays.asList(reader.readLine().split(","));
-            data=reader.lines().map(e->Arrays.asList(e.split(","))).collect(Collectors.toList());
-
+            indexOfIdColumn=columns.indexOf("id");
+            while((line=reader.readLine())!=null)
+            {
+                currentId=line.split(",")[indexOfIdColumn];
+                data.put(currentId,Arrays.asList(line.split(DELIMITER, -1)));
+            }
             switch (numberOfOperation)
             {
                 case 1: printTen(data,columns);
@@ -132,14 +150,13 @@ public class Main {
                     break;
                 case 4: printNamesAndOccurrences(data,columns);
                     break;
-                case 5: printNameAndCountParents(data,columns,additionalParameter);
+                case 5: printNameAndCountParents(data,columns,String.valueOf(additionalParameter));
                     break;
                 case 6: printMediumAge(data,columns);
                     break;
                 case 7: printRangesOfAge(data,columns);
                     break;
                 default:System.out.println("Choose operation from 1 to 7...");break;
-
             }
 
 
@@ -148,4 +165,6 @@ public class Main {
         }
 
     }
+
+
 }
